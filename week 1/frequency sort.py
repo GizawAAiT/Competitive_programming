9804059ec72b7bee468213cdf3ced9eaecd9924d
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2021-12-25 08:38:38 +0300

    hi

---
+++ week 1/frequency sort.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    freq=[0]*100
+    for i in arr:
+        
+        freq[i]+=1
+        
+    return freq    
+    # for i in range(100):
+    #     print(str(freq[i]),end=" ")    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
