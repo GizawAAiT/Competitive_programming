5439e0b5a3fb9d974824f279b37f8d7a23bfd342
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2022-05-16 10:31:53 +0300

    ritate array

---
+++ may 15-may 21/189. Rotate Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i  in range(k):
+            v=nums.pop()
+            nums.insert(0,v)
+        return nums
