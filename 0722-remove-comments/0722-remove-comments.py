519f8c1923b73637dd559d6593c18777e1bd33d1
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2022-12-30 18:49:44 +0300

    Time: 40 ms (68.09%), Space: 13.9 MB (44.72%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -1,20 +1,19 @@
 class Solution:
-    def removeComments(self, s: List[str]) -> List[str]:
-        s = '#'.join(s)
-        print(s)
+    def removeComments(self, source: List[str]) -> List[str]:
+        source = '#'.join(source)
         i = 0
-        while i <len(s)-1:
-            if s[i:i+2] =='/*':
+        while i <len(source)-1:
+            if source[i:i+2] =='/*':
                 j = i+2
-                while s[j:j+2]!='*/':j+=1
-                s = s[:i] + s[j+2:]
-            elif s[i:i+2] =='//':
+                while source[j:j+2]!='*/':j+=1
+                source= source[:i] + source[j+2:]
+            elif source[i:i+2] =='//':
                 j = i+2
-                while j<len(s) and s[j]!='#':j+=1 
-                s = s[:i] + s[j:]
+                while j<len(source) and source[j]!='#':j+=1 
+                source = source[:i] + source[j:]
             else: i+=1
        
-        s =s.split('#')
-        s = [i for i in s if len(i)>=1]
+        source =source.split('#')
+        source = [i for i in source if len(i)>=1]
         
-        return s
+        return source
