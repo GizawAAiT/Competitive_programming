a77aa284694f175c23a86d12f9dd9fa9287ea766
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2023-01-17 00:07:00 +0300

    Time: 128 ms (91.43%), Space: 14.8 MB (24.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -15,6 +15,7 @@ class Solution:
             if r in zero_in_row:
                 for c in range(len(matrix[0])):
                     matrix[r][c] =0
+                    
         for c in range(len(matrix[0])):
             if c in zero_in_col:
                 for r in range(len(matrix)):
