ab9980dd6dce57d07c6204fa51f61720da722ef7
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2023-02-23 16:16:19 +0300

    Time: 182 ms (39.95%), Space: 14 MB (51.27%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,11 +3,12 @@ class Solution:
         max_freq = 0
         left = 0
         dic = defaultdict(int)
-        result = 0
         
+        result = 0
         for right in range(len(s)):
             dic[s[right]] += 1
             max_freq = max(dic.values()) 
+            
             while right-left + 1 - max_freq > k:
                 dic[s[left]] -= 1
                 left +=1
