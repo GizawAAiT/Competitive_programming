75feff1fb7999a12f03c073cba9c2d408f845257
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2023-03-03 14:38:53 +0300

    Time: 44 ms (34.97%), Space: 13.9 MB (23.25%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,25 +5,47 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, head1, head2):
-        
-        if not head2: return head1
-        if not head1: return head2 
-        
-        dummy = ListNode()
-        t = dummy
-        while head1 and head2:
+        t = head = ListNode()
+        def mergeRecursive(head1, head2,t):
+            # base case
+            if not head1:
+                t.next = head2
+                t = t.next
+                return 
+            if not head2:
+                t.next = head1
+                t=t.next
+                return
+            
             if head1.val <= head2.val:
-                node = ListNode(head1.val) 
-                t.next = node
+                t.next = ListNode(head1.val) 
+                t=t.next
                 head1 = head1.next
             else:
-                node = ListNode(head2.val)
-                t.next = node
+                t.next = ListNode(head2.val)
+                t=t.next
                 head2 = head2.next
-            t= t.next
-        if  head1:
-            t.next = head1
-        if head2:
-            t.next = head2
-        return dummy.next
+            
+            return mergeRecursive(head1, head2,t)
+        mergeRecursive(head1, head2,t)
+        return head.next
+        
+        
+#         dummy = ListNode()
+#         t = dummy
+#         while head1 and head2:
+#             if head1.val <= head2.val:
+#                 node = ListNode(head1.val) 
+#                 t.next = node
+#                 head1 = head1.next
+#             else:
+#                 node = ListNode(head2.val)
+#                 t.next = node
+#                 head2 = head2.next
+#             t= t.next
+#         if  head1:
+#             t.next = head1
+#         if head2:
+#             t.next = head2
+#         return dummy.next
         
