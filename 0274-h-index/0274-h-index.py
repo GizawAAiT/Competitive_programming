79cca79717f911604bb38fe64c86c81c0096c5d9
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2023-03-06 17:08:26 +0300

    Time: 45 ms (36.35%), Space: 14.1 MB (63.86%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True) 
+        left = -1
+        right = len(citations)
+        while left<right-1:
+            mid = left + (right-left)//2
+            if mid+1 > citations[mid]:
+                right = mid
+            else:
+                left = mid
+        return right
