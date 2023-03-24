66e253680be4c9c1ff726b4cad91689fdd0a7518
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2023-03-24 17:54:37 +0300

    Time: 160 ms (27.30%), Space: 15.2 MB (67.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,19 @@
+'''Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.'''
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums) 
-        return int((n/2)*(n+1) - sum(nums))
+        
+        nums.append(-1)
+        def cycle_sort():
+            for indx in range(len(nums)):
+
+                while nums[indx] != indx : 
+                    if nums[indx] == -1: break
+                    nums[nums[indx]], nums[indx] = nums[indx], nums[nums[indx]]
+        
+        
+        cycle_sort()
+        
+        for i in range(len(nums)):
+            if nums[i] == -1:
+                return i
         
