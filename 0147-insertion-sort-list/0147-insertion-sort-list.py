64519a7596f6e5d0d01bc522c7f779ea5c550c83
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2023-04-04 18:49:52 +0300

    Time: 1537 ms (43.46%), Space: 16.5 MB (96.04%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.py
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -5,11 +5,6 @@
 #         self.next = next
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def printListNode(head):
-            while head:
-                print(head.val, '---> ', end = '')
-                head = head.next
-            print()
             
         dummy = cur = ListNode(float('-inf'))
         dummy.next = head
@@ -19,20 +14,15 @@ class Solution:
             node = cur.next
             cur.next = cur.next.next
             p = dummy
-            # print(node.val, '-->')
             while p != cur and p.next.val < node.val:
                 p = p.next
             
-            # print((cur.val, p.val))
             t = p.next
             p.next = node
             node.next = t
             
             if p == cur:
                 cur = cur.next
-            # t = dummy
-            # printListNode(t)
-        
         return dummy.next
     
 
