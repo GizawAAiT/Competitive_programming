7f9e9dd0bb3a65fb18e81c89c0d2064baf5b3265
Gizaw Dagne <gizawaait@gmail.com>
2023-04-18 22:01:02 +0300

    merge arrays

--- codeforces/A_Merging_Arrays.py
+++ codeforces/A_Merging_Arrays.py
@@ -4,6 +4,7 @@ nums1 = [int(_) for _ in input().split()]
 nums2 = [int(_) for _ in input().split()]
 i,j = 0,0 
 merged = []
+
 while i<n and j<m:
     if nums1[i]<nums2[j]:
         merged.append(nums1[i])
@@ -11,12 +12,13 @@ while i<n and j<m:
     else:
         merged.append(nums2[j])
         j+=1
+
 while i<n:
     merged.append(nums1[i])
     i+=1
+
 while j<m:
     merged.append(nums2[j])
     j+=1
-# for num in merged:
-#     print(num, end=' ')
+
 print(*merged)
