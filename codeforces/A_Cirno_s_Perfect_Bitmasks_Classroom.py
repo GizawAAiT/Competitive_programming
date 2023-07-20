fad7a5cb48ea1d8cec6711d4692bbe425dcc28ca
GizawAAiT <gizawdagnemogessie@gmail.com>
2023-07-20 22:30:20 +0300

    Carno's perfect Bitmasks classroom

--- codeforces/A_Cirno_s_Perfect_Bitmasks_Classroom.py
+++ codeforces/A_Cirno_s_Perfect_Bitmasks_Classroom.py
@@ -1,22 +1,14 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
-    d = 0
-    zero, one = 0, 0
-    while n and not one:
-        cur = n & 1
-        n >>= 1
-
-        if cur == 0 and not zero:
-            zero = 2 ** d
-
-        if cur == 1 and not one:
-            one = 2 ** d
-        d += 1
-
+    if n == 1:
+        print(3)
     
+    else:
+        i = 0
+        while not n & 1:
+            n >>= 1 
+            i += 1
+        
+        print(2**i + 1 if not n>>1 else 2**i)
 
-    if not n and not zero:
-        zero = 2 ** d
-
-    print(zero + one)
