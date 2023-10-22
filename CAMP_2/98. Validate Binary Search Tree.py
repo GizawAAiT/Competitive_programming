623258b70a8314b3de151a706064579b8e19d406
unknown <gizawdagnemogessie@gmail.com>
2023-10-22 22:17:17 +0300

    ቫሊዳተ ቢናርይ ሰአርችህ ትረእ

---
+++ CAMP_2/98. Validate Binary Search Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, lowerLim, upperLim):
+            if not node: return True
+            if not(lowerLim < node.val and upperLim > node.val): return False
+            return (valid(node.left, lowerLim, node.val) and valid(node.right, node.val, upperLim))
+        
+        return valid(root, float("-inf"), float("inf"))
+        
