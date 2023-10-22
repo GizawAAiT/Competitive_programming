44fa0a9d2cda9190d2a4e9b6e5c10691743eb2a1
unknown <gizawdagnemogessie@gmail.com>
2023-10-22 23:52:26 +0300

    Updated

--- CAMP_2/1041. Robot Bounded In Circle.py
+++ CAMP_2/1041. Robot Bounded In Circle.py
@@ -9,17 +9,15 @@ class Solution:
 
         direction = 90
         x, y = 0, 0
-        for i in range(1000):
-            for ins in instructions:
-                if ins == 'L':
-                    direction = (direction - 90) % 360
-                elif ins == 'R':
-                    direction = (direction + 90) % 360
-                else:
-                    x, y = move(direction, x, y)
-            if x == 0 and y == 0 and direction == 90: 
-                return True 
-        return False
+       
+        for ins in instructions:
+            if ins == 'L':
+                direction = (direction - 90) % 360
+            elif ins == 'R':
+                direction = (direction + 90) % 360
+            else:
+                x, y = move(direction, x, y)
+        return (x,y) == (0, 0) or direction != 90
 
 
 
