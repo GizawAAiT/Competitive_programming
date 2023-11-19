211b8acab2898035e859314c9218af6f3fc28542
Gizaw Dagne <95862942+GizawAAiT@users.noreply.github.com>
2023-11-19 10:37:49 +0300

    Added solution for Sum of Left Leaves

---
+++ camp/week1/sum-of-left-leaves.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root):
+        q, res = [root], 0
+        while q:
+            node = q.pop(0)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+            if node.left and not node.left.left and not node.left.right:
+                res += node.left.val
+        return res
+                    
+
